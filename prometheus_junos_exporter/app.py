fca4e442890de756e4efd5bf4ae9058154d57cec
Cellebyte <marcel.fest@live.de>
2018-05-01 00:28:53 +0200

    Wrap gunicorn into my metriks definitions.

---
+++ prometheus_junos_exporter/app.py
@@ -0,0 +1,257 @@
+from jnpr.junos import Device
+import re
+from cgi import escape, parse_qs
+import json
+import yaml
+import logging
+import getpass
+from prometheus_junos_exporter import wrapping
+from prometheus_junos_exporter import custom_junos as junos
+import os
+logger = logging.getLogger(__name__)
+
+wrapping.init()
+config = None
+
+
+class Metrics(object):
+    """
+    Store metrics and do conversions to PromQL syntax
+    """
+
+    class _Metric(object):
+        """
+        This is an actual metric entity
+        """
+
+        def __init__(self, name, value, metric_type, labels=None):
+            self.name = name
+            self.value = float(value)
+            self.metric_type = metric_type
+            self.labels = []
+            if labels:
+                for label_name, label_value in labels.items():
+                    self.labels.append(
+                        '{}="{}"'.format(label_name, label_value))
+
+        def __str__(self):
+            return "{}{} {}".format(self.name, "{" + ",".join(self.labels) + "}", self.value)
+
+    def __init__(self):
+        self._metrics_registry = {}
+        self._metric_types = {}
+
+    def register(self, name, metric_type):
+        """
+        Add a metric to the registry
+        """
+        if self._metrics_registry.get(name) is None:
+            self._metrics_registry[name] = []
+            self._metric_types[name] = metric_type
+        else:
+            raise ValueError(
+                'Metric named {} is already registered.'.format(name))
+
+    def add_metric(self, name, value, labels=None):
+        """
+        Add a new metric
+        """
+        collector = self._metrics_registry.get(name)
+        if collector is None:
+            raise ValueError('Metric named {} is not registered.'.format(name))
+
+        metric = self._Metric(name, value, self._metric_types[name], labels)
+        collector.append(metric)
+
+    def collect(self):
+        """
+        Collect all metrics and return
+        """
+        lines = []
+        for name, metric_type in self._metric_types.items():
+            lines.append("# TYPE {} {}".format(name, metric_type))
+            lines.extend(self._metrics_registry[name])
+        return "\n".join([str(x) for x in lines]) + '\n'
+
+
+def hello(environ, start_response):
+    """Like the example above, but it uses the name specified in the
+URL."""
+    # get the name from the url if it was specified there.
+    args = environ['myapp.url_args']
+    if args:
+        subject = escape(args[0])
+    else:
+        subject = 'World'
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ['''Hello %(subject)s
+            Hello %(subject)s!
+
+''' % {'subject': subject}]
+
+
+def not_found(environ, start_response):
+    """Called if no URL matches."""
+    start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
+    return [bytes('Not Found', 'utf-8')]
+
+
+def get_interface_metrics(registry, dev, hostname, access=True):
+    """
+    Get interface metrics
+    """
+    # interfaces
+    interfaces = {}
+    if access:
+        interfaces = junos.get_specific_ports_information(
+            dev, "[afgxe][et]-*/1/*")
+    else:
+        interfaces = junos.get_all_ports_information(dev)
+
+    for MetricName, MetricFamily in wrapping.METRICS.items():
+        for metrik_def in wrapping.NETWORK_METRICS.get(MetricName, []):
+            metrik_name, key, function, _ = wrapping.create_metrik_params(
+                metrik_def)
+            metrik_name = "{}_{}_{}".format(wrapping.METRICS_BASE.get(
+                'base', 'junos'), wrapping.METRICS_BASE.get('interface', 'interface'), metrik_name)
+            registry.register(metrik_name, MetricFamily)
+            for interface, metriken in interfaces.items():
+                if metriken.get(key) is not None:
+                    labels_data = {'hostname': hostname,
+                                   'interface': interface}
+                    labels_variable = {label['key']: metriken.get(
+                        label['key'], "") for label in wrapping.NETWORK_LABEL_WRAPPER}
+                    labels = {**labels_data, **labels_variable}
+                    wrapping.create_metrik(metrik_name,
+                                           registry, key, labels, metriken, function=function)
+
+
+def get_environment_metrics(registry, dev, hostname):
+    """
+    Get environment metrics
+    """
+    environment = junos.get_environment(dev)
+    for MetricName, MetricFamily in wrapping.METRICS.items():
+        for metrik_def in wrapping.ENVIRONMENT_METRICS.get(MetricName, []):
+            metrik_name, key, function, specific = wrapping.create_metrik_params(
+                metrik_def)
+            metrik_name = "{}_{}_{}".format(
+                wrapping.METRICS_BASE['base'], wrapping.METRICS_BASE['device'], metrik_name)
+            registry.register(metrik_name, MetricFamily)
+            labels_data = {'hostname': hostname}
+            labels_variable = {label['key']: environment.get(
+                label['key'], "") for label in wrapping.ENVIRONMENT_LABEL_WRAPPER}
+            labels = {**labels_data, **labels_variable}
+            if specific and function:
+                data = environment.get(key, None)
+                wrapping.FUNCTIONS[function](
+                    metrik_name, registry, labels, data, create_metrik=wrapping.create_metrik)
+            elif environment.get(key):
+                wrapping.create_metrik(
+                    metrik_name, registry, key, labels, environment, function=function)
+    print("Change all metrics")
+
+
+def get_bgp_metrics(registry, dev, hostname):
+    """
+    Get BGP neighbor metrics
+    """
+    bgp = junos.get_bgp_information(dev)
+    for MetricName, MetricFamily in wrapping.METRICS.items():
+        for metrik_def in wrapping.BGP_METRICS.get(MetricName, []):
+            metrik_name, key, function, specific = wrapping.create_metrik_params(
+                metrik_def)
+            metrik_name = "{}_{}_{}".format(
+                wrapping.METRICS_BASE['base'], wrapping.METRICS_BASE['bgp'], metrik_name)
+            registry.register(metrik_name, MetricFamily)
+            if bgp:
+                for peername, metriken in bgp.items():
+                    if metriken.get(key) is not None:
+                        labels_data = {'hostname': hostname,
+                                       'peername': peername}
+                        labels_variable = {label['key']: metriken.get(
+                            label['key'], "") for label in wrapping.BGP_LABEL_WRAPPER}
+                        labels = {**labels_data, **labels_variable}
+                        wrapping.create_metrik(
+                            metrik_name, registry, key, labels, metriken, function=function)
+
+
+def metrics(environ, start_response):
+
+    # load config
+    CONF_DIR = os.path.join('/etc', 'prometheus-junos-exporter')
+    with open(os.path.join(CONF_DIR, 'config.yml'), 'r') as f:
+        config = yaml.load(f)
+
+    # parameters from url
+    parameters = parse_qs(environ.get('QUERY_STRING', ''))
+
+    # get profile from config
+    profile = config[parameters.get('module', ['default'])[0]]
+    hostname = parameters['target'][0]
+    # open device connection
+    if profile['auth']['method'] == 'password':
+        # using regular username/password
+        dev = Device(host=hostname,
+                     user=profile['auth'].get('username', getpass.getuser()),
+                     password=profile['auth'].get('password', getpass.getpass()),
+                     port=profile['auth'].get('port', 22))
+    elif profile['auth']['method'] == 'ssh_key':
+        # using ssh key
+        dev = Device(host=hostname,
+                     user=profile['auth'].get('username', getpass.getuser()),
+                     ssh_private_key_file=profile['auth'].get(
+                         'ssh_key', '~/.ssh/id_rsa'),
+                     port=profile['auth'].get('port', 22),
+                     password=profile['auth'].get('password', None))
+    dev.open()
+    # create metrics registry
+    registry = Metrics()
+
+    # get and parse metrics
+    types = profile['metrics']
+    if 'interface' in types:
+        get_interface_metrics(registry, dev, hostname, access=False)
+    if 'interface_specifics' in types:
+        get_interface_metrics(registry, dev, hostname, access=True)
+    if 'environment' in types:
+        get_environment_metrics(registry, dev, hostname)
+    if 'bgp' in types:
+        get_bgp_metrics(registry, dev, hostname)
+
+    # start response
+    data = registry.collect()
+    print(data)
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain')
+    ]
+    start_response(status, response_headers)
+    return [bytes(data, 'utf-8')]
+
+
+# map urls to functions
+urls = [
+    # (r'metrics$', self_service),
+    # (r'metrics/$', self_service),
+    (r'metrics/?$', metrics),
+    (r'metrics/(.+)$', metrics)
+]
+
+
+def app(environ, start_response):
+    """
+    The main WSGI application. Dispatch the current request to
+    the functions from above and store the regular expression
+    captures in the WSGI environment as  `myapp.url_args` so that
+    the functions from above can access the url placeholders.
+
+    If nothing matches call the `not_found` function.
+    """
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    for regex, callback in urls:
+        match = re.search(regex, path)
+        if match is not None:
+            environ['app.url_args'] = match.groups()
+            return callback(environ, start_response)
+    return not_found(environ, start_response)
---
+++ prometheus_junos_exporter/wrapping.py
@@ -0,0 +1,166 @@
+
+#!/usr/bin/env python3
+import sys
+import os
+import time
+import datetime
+from getpass import getpass, getuser
+import yaml
+from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily
+# If u want to have more metrics. You must edit the config/metrics_definitions.yml
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config', 'metrics_definition.yml'), 'r') as metrics_definitions:
+    DEFINITIONS = yaml.load(metrics_definitions).get('DEFINITIONS', {})
+
+# Get the Metrics DEFINITIONS
+METRICS_BASE = DEFINITIONS.get('METRICS_BASE', {})
+NETWORK_REGEXES = DEFINITIONS.get('NETWORK_REGEXES', [])
+NETWORK_METRICS = DEFINITIONS.get('NETWORK_METRICS', {})
+ENVIRONMENT_METRICS = DEFINITIONS.get('ENVIRONMENT_METRICS', {})
+NETWORK_LABEL_WRAPPER = DEFINITIONS.get('NETWORK_LABEL_WRAPPER', [])
+ENVIRONMENT_LABEL_WRAPPER = DEFINITIONS.get('ENVIRONMENT_LABEL_WRAPPER', [])
+BGP_METRICS = DEFINITIONS.get('BGP_METRICS', {})
+BGP_LABEL_WRAPPER = DEFINITIONS.get('BGP_LABEL_WRAPPER', [])
+
+global FUNCTIONS
+def init():
+    global FUNCTIONS, METRICS
+    FUNCTIONS = {
+        'is_ok': is_ok,
+        'flap': flap,
+        'temp': temp,
+        'intify': intify,
+        'floatify': floatify,
+        'fan_power_temp_status': fan_power_temp_status,
+        'temp_celsius': temp_celsius,
+        'reboot': reboot,
+        'cpu_idle': cpu_idle,
+        'cpu_usage': cpu_usage,
+        'ram': ram,
+        'ram_usage': ram_usage
+    }
+
+    METRICS = {
+        'Counter': 'counter',
+        'Gauge': 'gauge'
+    }
+
+def is_ok(boolean):
+    if isinstance(boolean, bool):
+        if boolean:
+            return 1
+        return 0
+    elif isinstance(boolean, str):
+        if boolean.lower() in ['up', 'ok', 'established']:
+            return 1
+        return 0
+    else:
+        raise Exception("Unknown Type")
+
+
+def flap(string):
+    if 'never' in string.lower():
+        return 0
+    string = string.split(" (")[0]
+    val = time.mktime(datetime.datetime.strptime(
+        string, "%Y-%m-%d %H:%M:%S %Z").timetuple())
+    return val
+
+
+def temp(string):
+    string = int(string.split(' ')[0])
+    return string
+
+
+def intify(string):
+    return int(string)
+
+
+def floatify(string):
+    return float(string)
+
+
+def boolify(string):
+    return 'true' in string.lower()
+
+
+def fan_power_temp_status(metrik, registry, labels, data, create_metrik=None):
+    for sensorname, information in data.items():
+        labels['sensorname'] = sensorname
+        create_metrik(metrik, registry, 'status' , labels,
+                               information, function='is_ok')
+
+
+def temp_celsius(metrik, registry, labels, data, create_metrik=None):
+    for sensorname, information in data.items():
+        labels['sensorname']=sensorname
+        create_metrik(metrik, registry, 'temperature',
+                               labels, information, function='intify')
+
+
+def reboot(metrik, registry, labels, data, create_metrik=None):
+    reason_string = data['last_reboot_reason']
+    reason = 1
+    for a in ["failure", "error", "failed"]:
+        if a in reason_string.lower():
+            reason = 0
+    labels['reboot_reason'] = reason_string
+    registry.add_metric(metrik, reason, labels=labels)
+
+
+def cpu_usage(metrik, registry, labels, data, create_metrik=None):
+    for slot, perf in data.items():
+        label = "cpu_{}".format(str(slot))
+        labels['cpu'] = label
+        cpu_usage = 100 - int(perf['cpu-idle'])
+        registry.add_metric(metrik, cpu_usage, labels=labels)
+
+
+def cpu_idle(metrik, registry, labels, data, create_metrik=None):
+    for slot, perf in data.items():
+        label = "cpu_{}".format(str(slot))
+        labels['cpu']=label
+        cpu_idle = int(perf['cpu-idle'])
+        registry.add_metric(metrik, cpu_idle, labels=labels)
+
+
+def ram_usage(metrik, registry, labels, data, create_metrik=None):
+    for slot, perf in data.items():
+        label = "ram_{}".format(str(slot))
+        labels['ram'] = label
+        memory_complete = perf['memory-dram-size'].lower().replace("mb",
+                                                                   "").strip()
+        memory_complete = int(memory_complete)
+        memory_usage = int(perf['memory-buffer-utilization'])
+        memory_bytes_usage = (memory_complete * memory_usage / 100) * 1049000
+        registry.add_metric(metrik, memory_bytes_usage, labels=labels)
+
+def ram(metrik, registry, labels, data, create_metrik=None):
+    for slot, perf in data.items():
+        label = "ram_{}".format(str(slot))
+        labels['ram'] = label
+        memory_complete = perf['memory-dram-size'].lower().replace("mb","").strip()
+        memory_complete = int(memory_complete)
+        memory_bytes = memory_complete * 1049000
+        registry.add_metric(metrik, memory_bytes, labels=labels)
+
+def create_metrik_params(metrik_def, call='interfaces'):
+    metrik_name = metrik_def['metrik']
+    key = metrik_def['key']
+    # description = metrik_def.get('description', '')
+    type_of = metrik_def.get('type', None)
+    function = metrik_def.get('function', None)
+    specific = metrik_def.get('specific', False)
+    if type_of:
+        metrik_name = '{}_{}'.format(metrik_name, type_of)
+    return metrik_name, key, function, specific
+
+def create_metrik(metrik_name, registry, key, labels, metriken, function=None):
+    if metriken.get(key) is not None:
+        try:
+            if function:
+                registry.add_metric(metrik_name, FUNCTIONS[function](metriken.get(key)), labels=labels)
+            else:
+                registry.add_metric(metrik_name, metriken.get(key), labels=labels)
+        except (ValueError, KeyError) as e:
+            print("Error :: {}".format(e))
+
