aecc8587f70234a986a30cab204dc7ed24ef3047
Cellebyte <marcel.fest@live.de>
2018-05-04 00:18:29 +0200

    Remove console-scripts

--- setup.py
+++ setup.py
@@ -98,9 +98,4 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'prometheus-junos-exporter=prometheus_junos_exporter.main:main',
-        ],
-    },
 )
