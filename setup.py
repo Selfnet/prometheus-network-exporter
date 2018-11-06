32f5ad0136430f8b25a19eedfc99a1fe192647d3
Cellebyte <marcel.fest@live.de>
2018-11-06 23:38:18 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['PyYAML', 'tornado', 'junos-eznc', 'arubaos_client', 'unifi_client' 'voluptuous'],
+    install_requires=['PyYAML', 'tornado', 'junos-eznc', 'arubaos_client', 'unifi_client', 'voluptuous'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
