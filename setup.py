f457a8e7aa8ab5ba4c84f27a5bf5dec9e2793132
Cellebyte <marcel.fest@live.de>
2019-01-21 22:59:42 +0100

    Fixed setup.py using wrong package

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['PyYAML', 'tornado', 'junos-eznc', 'arubaos_client', 'unifi_client', 'voluptuous', 'fqdn', 'prometheus_client'],
+    install_requires=['PyYAML', 'tornado', 'junos-eznc', 'arubaos_client', 'unifi_client', 'voluptuous', 'fqdn', 'prometheus-client'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
