fc4b4aff47256e690413f2e0f1dd242805396bb4
Cellebyte <marcel.fest@live.de>
2019-02-10 23:04:49 +0100

    Reload Facts on environment Gathering

--- prometheus_network_exporter/devices/junosdevice.py
+++ prometheus_network_exporter/devices/junosdevice.py
@@ -24,8 +24,8 @@ class JuniperNetworkDevice(basedevice.Device):
                         ssh_private_key_file=ssh_private_key_file,
                         ssh_config=ssh_config,
                         password=password,
-                        port=port)
-        # gather_facts=False)
+                        port=port,
+                        gather_facts=False)
         super().__init__(hostname, device)
 
     def get_bgp(self):
@@ -72,6 +72,7 @@ class JuniperNetworkDevice(basedevice.Device):
         return result
 
     def get_environment(self):
+        self.device.facts_refresh()
         facts = self.device.facts
         uptime = self.device.uptime
         rengine = RoutingEngineTable(self.device).get()
